c216a5fe0926e5f2ae6f1d6b05722376088806c0
ekaitzhara <ekaitzhara16@gmail.com>
2020-03-21 13:12:24 +0100

    GetBaselineModel hasita

--- src/entregatzeko/GetBaselineModel.java
+++ src/entregatzeko/GetBaselineModel.java
@@ -1,8 +1,13 @@
 package entregatzeko;
 
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class GetBaselineModel {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		if(args.length == 0) {
 			System.out.println("LAGUNTZA");
@@ -10,7 +15,23 @@ public class GetBaselineModel {
 			System.exit(0);
 		}
 		
+		baselineSortu(args[0], args[1], args[2]);
 	}
 	
+	public static void baselineSortu(String trainArff, String testArff, String modelPath) throws Exception {
+		DataSource source = new DataSource(trainArff);
+		Instances train = source.getDataSet();
+		if (train.classIndex() == -1)
+			train.setClassIndex(train.numAttributes() - 1);
+		
+		
+		DataSource sourceTest = new DataSource(testArff);
+		Instances test = sourceTest.getDataSet();
+		if (test.classIndex() == -1)
+			test.setClassIndex(0);
+		
+		NaiveBayes classifier = new NaiveBayes();
+		Evaluation evaluator = new Evaluation(train);
+	}
 	
 }

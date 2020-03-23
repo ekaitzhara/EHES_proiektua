58a2f753696ef30215b64e91305bb6aebacadaa9
ekaitzhara <ekaitzhara16@gmail.com>
2020-03-23 12:42:40 +0100

    GetBaselineModel amaituta

--- src/entregatzeko/GetBaselineModel.java
+++ src/entregatzeko/GetBaselineModel.java
@@ -8,6 +8,7 @@ import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
+import weka.core.Utils;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.instance.RemovePercentage;
@@ -44,43 +45,40 @@ public class GetBaselineModel {
 		String errepresentazioa = "BOW";
 		String bektoreMota = "NonSparse";
 		
-		for (int i = 0; i < 11; i++) {
-			int seed = 1;
-			dataSet.randomize(new Random(seed));
-			RemovePercentage removePercentage = new RemovePercentage();
-			
-			// Train zatia lortzeko
-			removePercentage.setInputFormat(dataSet);
-			removePercentage.setPercentage(70);
-			removePercentage.setInvertSelection(true);	// %70-a lortzeko
-			Instances train = Filter.useFilter(dataSet, removePercentage);
-			
-			// Dev zatia lortzeko
-			removePercentage.setInputFormat(dataSet);
-			removePercentage.setInvertSelection(false);
-			Instances dev = Filter.useFilter(dataSet, removePercentage);
-			
-			String[] aux = arffPath.split("/");
-			String direktorioa = arffPath.replace(aux[aux.length-1],"");
-			String dictionaryPath = direktorioa + "/train_" + errepresentazioa + "_" + bektoreMota + "_dictionary.txt";
-			
-			Instances train_BOW = TransformRaw.transformRawInstances(train, errepresentazioa, bektoreMota, dictionaryPath);
-			
-			Instances dev_BOW = MakeCompatible.makeCompatibleInstances(dev, dictionaryPath);
-			
-			Instances train_BOW_FSS = FSS_InfoGain.atributuenHautapenaInstances(train_BOW);
-			
-			Instances dev_BOW_FSS = FSS_MakeCompatible.make2InstancesCompatibles(train_BOW_FSS, dev_BOW);
-			
-			
-		}
+		int seed = 1;
+		dataSet.randomize(new Random(seed));
+		RemovePercentage removePercentage = new RemovePercentage();
 		
+		// Train zatia lortzeko
+		removePercentage.setInputFormat(dataSet);
+		removePercentage.setPercentage(70);
+		removePercentage.setInvertSelection(true);	// %70-a lortzeko
+		Instances train = Filter.useFilter(dataSet, removePercentage);
 		
+		// Dev zatia lortzeko
+		removePercentage.setInputFormat(dataSet);
+		removePercentage.setInvertSelection(false);
+		Instances dev = Filter.useFilter(dataSet, removePercentage);
 		
-		/*
+		String[] aux = arffPath.split("/");
+		String direktorioa = arffPath.replace(aux[aux.length-1],"");
+		String dictionaryPath = direktorioa + "/train_" + errepresentazioa + "_" + bektoreMota + "_dictionary.txt";
+		
+		Instances train_BOW = TransformRaw.transformRawInstances(train, errepresentazioa, bektoreMota, dictionaryPath);
+		
+		Instances dev_BOW = MakeCompatible.makeCompatibleInstances(dev, dictionaryPath);
+		
+		Instances train_BOW_FSS = FSS_InfoGain.atributuenHautapenaInstances(train_BOW);
+		
+		Instances dev_BOW_FSS = FSS_MakeCompatible.make2InstancesCompatibles(train_BOW_FSS, dev_BOW);
+		
+		evaluator = new Evaluation(train_BOW_FSS);
+		classifier.buildClassifier(train_BOW_FSS);
+		evaluator.evaluateModel(classifier, dev_BOW_FSS);
+		
 		// Model karpeta ez badago sortuta
-		String[] aux = modelPath.split("/");
-		String modelName = aux[aux.length-1];
+		String[] auxModel = modelPath.split("/");
+		String modelName = auxModel[auxModel.length-1];
         File modelDirectory = new File(modelPath.replace(modelName, ""));
         if (!modelDirectory.exists())
         	modelDirectory.mkdir();
@@ -97,7 +95,26 @@ public class GetBaselineModel {
 		System.out.println(evaluator.toSummaryString("\n=== SUMMARY ===", false));
 		System.out.println(evaluator.toClassDetailsString());
 		System.out.println(evaluator.toMatrixString());
-		*/
+		
+	}
+	
+	
+	public static int klaseMinoritarioaLortu(Instances dataSet) {
+		int klaseMinoritarioa = Utils.minIndex(dataSet.attributeStats(dataSet.classIndex()).nominalCounts);
+		if (klaseMinoritarioa == 0) {
+			int[] classCounts = dataSet.attributeStats(dataSet.classIndex()).nominalCounts;
+			int min = Integer.MAX_VALUE;
+			int min_pos = -1;
+			for (int i = 0; i < classCounts.length; i++) {
+				if ((classCounts[i] < min) && (classCounts[i] != 0)) {
+					min = classCounts[i];
+					min_pos = i;
+				}
+			}
+			klaseMinoritarioa = min_pos;
+		}
+		System.out.println("Klase minoritarioa => " + klaseMinoritarioa);
+		return klaseMinoritarioa;
 	}
 	
 }

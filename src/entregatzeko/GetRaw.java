6e7f196adb2c4386b144c595d8a0a041bf6991d2
ekaitzhara <ekaitzhara16@gmail.com>
2020-04-05 16:12:02 +0200

    GetRaw jar

--- src/entregatzeko/GetRaw.java
+++ src/entregatzeko/GetRaw.java
@@ -44,96 +44,22 @@ public class GetRaw {
 	
 	public static void datuGordinetikArff(String pathToData, String pathArff) throws Exception {
 		
-		
-		String dataName = pathToData.split("\\.")[0];
-		String fileType = pathToData.split("\\.")[1];
-		String pathFileAux = dataName + "2." + fileType;
-		
-		System.out.println(pathToData + " fitxategia aldatzen");
-		
-		// Arazoak ematen dituzten karaktereak kendu
-		FileWriter fw = new FileWriter(pathFileAux);
-        BufferedReader br = new BufferedReader(new FileReader(pathToData)); 
-        String line;
-        while((line = br.readLine()) != null) {
-            line = line.replace("'","`");
-            fw.write(line + "\n");
-        }
-        fw.close();
-
-        Instances data = null;
-        // Gure datuak csv moduan daude
-        if ("csv".equals(fileType)) {
-		    // load CSV
-		    CSVLoader loader = new CSVLoader();
-		    loader.setSource(new File(pathFileAux));
-		    data = loader.getDataSet();
-        }
-        
-        String relationName = data.relationName().substring(0, data.relationName().length()-1);
-        
-        Remove r = new Remove();
-	    int[] indice = {0};
-	    r.setAttributeIndicesArray(indice);
-	    r.setInvertSelection(false);
-	    r.setInputFormat(data);
-	    data = Filter.useFilter(data, r);
-	    
-	    NominalToString filterString = new NominalToString();
-		filterString.setAttributeIndexes("first");
-		filterString.setInputFormat(data);
-		data = Filter.useFilter(data, filterString);
-		System.out.println("Arff-ko instantzia => " + data.instance(2));
+		Instances data = GetRaw.datuGordinetikInstances(pathToData);
         
 		// Arff karpeta ez badago sortuta
 		String[] aux = pathArff.split("/");
 		String modelName = aux[aux.length-1];
         File modelDirectory = new File(pathArff.replace(modelName, ""));
         if (!modelDirectory.exists())
-        	modelDirectory.mkdir();
-		
-        if(data.classIndex() == -1)
-        	data.setClassIndex(data.numAttributes() - 1);
-        
-        RenameAttribute rename = new RenameAttribute();
-        rename.setAttributeIndices("last");
-        rename.setReplace("klasea");
-        rename.setInputFormat(data);
-        data = Filter.useFilter(data, rename);
-        System.out.println(data.classAttribute().name());
-        
-        System.out.println(data.numClasses());
-        if (data.numClasses() == 1) {
-        	NumericToNominal numToNom = new NumericToNominal();
-        	numToNom.setAttributeIndices("last");
-        	numToNom.setInputFormat(data);
-        	data = Filter.useFilter(data, numToNom);
-        	
-        	Remove remove = new Remove();
-        	remove.setAttributeIndices("last");
-        	remove.setInputFormat(data);
-        	data = Filter.useFilter(data, remove);
-        	
-        	Add addNominal = new Add();
-        	addNominal.setAttributeIndex("last");
-        	addNominal.setAttributeName("klasea");
-        	addNominal.setNominalLabels("NUM,LOC,HUM,DESC,ENTY,ABBR");
-        	addNominal.setInputFormat(data);
-        	data = Filter.useFilter(data, addNominal);
-        }
+        	modelDirectory.mkdir();        
         
         // save ARFF
-        data.setRelationName(relationName);
         ArffSaver saver = new ArffSaver();
         saver.setInstances(data);
         saver.setFile(new File(pathArff));
         saver.setDestination(new File(pathArff));
         saver.writeBatch();
         System.out.println(pathArff + " fitxategia-a ondo sortua");
-		System.out.println("------------------");
-		
-        File csv2 = new File(pathFileAux);
-        csv2.delete();
 			        
 	}
 	
